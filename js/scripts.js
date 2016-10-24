a294c77cb48772663ca3a19878f684dfe03fc461
Erica Nafziger and Luke Philips <erica@>
2016-10-24 12:02:58 -0700

    prints items into uppercase list, does not support blanks

--- js/scripts.js
+++ js/scripts.js
@@ -5,15 +5,29 @@ $(document).ready(function(){
  $("#blanks form").submit(function(event){
 
    blanks.forEach(function(item){// Loop on blank array to create users array
-     var userInput = $("input#"+item).val();
-     userInputList.push(userInput);
-     userInputList.sort();     
-   });
 
-console.log(userInputList);
-  //   list.map(function(item){
-  //    return item.toUpperCase()
-  //  });
+    //  if (item1, item2, item3) {
+     if (item1 === "") {
+       console.log("no data");
+     }
+     else {
+
+
+       var userInput = $("input#"+item).val();
+       userInputList.push(userInput);
+     };
+
+     userInputList.sort();
+
+   });
+   var outputList = userInputList.map(function(item) {
+     return item.toUpperCase();
+     console.log(outputList);
+   });
+   outputList.forEach(function(item) {
+     $("#show ul").append("<li>"+item+"</li>")
+
+   });
 
    $("#show").show();
    event.preventDefault();
